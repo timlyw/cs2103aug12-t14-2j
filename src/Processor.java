e37ae6bebccb93d4bfb7c7a58f1e990508555395
Shekhar Macintosh <brshekhar1@gmail.com>
2012-09-18 16:58:42 +0800

    Processor class outline

--- src/Processor.java
+++ src/Processor.java
@@ -1,10 +1,23 @@
 
 public class Processor {
+	
+	private Command userCommand;
+	private List<Tasks> matchedTasks;
+	private CommandParser commandParser;
+	
+	
 	public String getCommandFeedback(String command) {
 		return "Command feedback for " + command;
 	}
 	
 	public String executeCommand(String command) {
-		return "Command executed: " + command;
+		Command userCommand = commandParser.getParsedCommand(command);	
+		String screenOutput=processCommand(userCommand);
+		return screenOutput;
+	}
+	
+	private String processCommand(Command userCommand)
+	{
+		
 	}
 }
