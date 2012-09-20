3efd8330caf495566fb7370874da743e4b98e1ca
timlyw <timlyw@localhost>
2012-09-20 17:26:11 +0800

    Task Objects created

--- src/mhs/src/Task.java
+++ src/mhs/src/Task.java
@@ -1,11 +1,10 @@
 package mhs.src;
 
-import java.util.LinkedHashMap;
 import java.util.Map;
 
 import org.joda.time.DateTime;
 
-public class Task {
+public abstract class Task {
 
 	enum TaskCategory {
 		FLOATING("FLOATING"), TIMED("TIMED"), DEADLINE("DEADLINE");
@@ -20,17 +19,15 @@ public class Task {
 		}
 	}
 
-	private Integer taskId;
-	private String taskName;
-	private TaskCategory taskCategory;
-	private DateTime startDateTime;
-	private DateTime endDateTime;
-	private DateTime taskCreated;
-	private DateTime taskUpdated;
-	private DateTime taskLastSync;
-	private String gCalTaskId;
-	private Boolean isDone;
-	private Boolean isDeleted;
+	protected Integer taskId;
+	protected String taskName;
+	protected TaskCategory taskCategory;
+	protected DateTime taskCreated;
+	protected DateTime taskUpdated;
+	protected DateTime taskLastSync;
+	protected String gCalTaskId;
+	protected Boolean isDone;
+	protected Boolean isDeleted;
 
 	/**
 	 * 
@@ -46,16 +43,13 @@ public class Task {
 	 * @param isDone
 	 * @param isDeleted
 	 */
-	public Task(int taskId, String taskName, String taskCategory,
-			DateTime startDt, DateTime endDt, DateTime createdDt,
+	public Task(int taskId, String taskName, String taskCategory, DateTime createdDt,
 			DateTime updatedDt, DateTime syncDt, String gCalTaskId,
 			boolean isDone, boolean isDeleted) {
-				
+
 		setTaskId(taskId);
 		setTaskName(taskName);
 		setTaskCategory(taskCategory);
-		setStartDateTime(startDt);
-		setEndDateTime(endDt);
 		setTaskCreated(createdDt);
 		setTaskUpdated(updatedDt);
 		setTaskLastSync(syncDt);
@@ -85,49 +79,12 @@ public class Task {
 
 		setTaskId(taskId);
 		setTaskName(taskName);
-		setStartDateTime(startDt);
-		setEndDateTime(endDt);
 		setTaskCreated(createdDt);
 		setTaskUpdated(updatedDt);
 		setTaskLastSync(syncDt);
 		setgCalTaskId(gCalTaskId);
 		setDone(isDone);
 		setDeleted(isDeleted);
-	}
-
-	/*
-	 * Display Methods
-	 */
-	public String toString() {
-		// TODO
-		return null;
-	}
-
-	/*
-	 * Record methods
-	 */
-	/**
-	 * Return task properties for record file insertion
-	 * 
-	 * @return
-	 */
-	public Map<String, String> getTaskProperties() {
-
-		Map<String, String> taskProperties = new LinkedHashMap<String, String>();
-
-		taskProperties.put("taskId", taskId.toString());
-		taskProperties.put("taskName", taskName);
-		taskProperties.put("taskCategory", taskCategory.getValue());
-		taskProperties.put("startDateTime", startDateTime.toString());
-		taskProperties.put("endDateTime", endDateTime.toString());
-		taskProperties.put("taskCreated", taskCreated.toString());
-		taskProperties.put("taskUpdated", taskUpdated.toString());
-		taskProperties.put("taskLastSync", taskLastSync.toString());
-		taskProperties.put("gCalTaskId", gCalTaskId);
-		taskProperties.put("isDone", isDone.toString());
-		taskProperties.put("isDeleted", isDeleted.toString());
-
-		return taskProperties;
 	}
 
 	/*
@@ -148,22 +105,6 @@ public class Task {
 
 	public void setTaskName(String taskName) {
 		this.taskName = taskName;
-	}
-
-	public DateTime getStartDateTime() {
-		return startDateTime;
-	}
-
-	public void setStartDateTime(DateTime startDateTime) {
-		this.startDateTime = startDateTime;
-	}
-
-	public DateTime getEndDateTime() {
-		return endDateTime;
-	}
-
-	public void setEndDateTime(DateTime endDateTime) {
-		this.endDateTime = endDateTime;
 	}
 
 	public DateTime getTaskUpdated() {
@@ -235,8 +176,12 @@ public class Task {
 		}
 	}
 
-	public void setTaskCategory(TaskCategory taskCategory) {
-		this.taskCategory = taskCategory;
+	public DateTime getStartDateTime() {
+		return null;
+	}
+
+	public Map<String, String> getTaskProperties() {
+		return null;
 	}
 
 }
