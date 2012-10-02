e6a90ce2b67737c5901e96602900a20245837e35
Shekhar Baggavalli <brshekhar1@gmail.com>
2012-10-02 16:57:00 +0800

    fixed deadline task, edited display statements

---
+++ src/mhs/src/CommandParser.java
@@ -0,0 +1,132 @@
+package mhs.src;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+import org.joda.time.LocalDate;
+import org.joda.time.LocalTime;
+
+public class CommandParser {
+
+	public static Command getParsedCommand(String process) {
+
+		DateExtractor dateParser = new DateExtractor();
+		TimeExtractor timeParser = new TimeExtractor();
+		CommandExtractor commandParser= new CommandExtractor();
+		NameExtractor nameParser = new NameExtractor();
+
+		boolean taskNameFlag = false;
+		boolean timeFlag = false;
+		boolean dateFlag= false;
+		
+		String command = null;
+		String taskName = null;
+		String edittedName = null;
+		String tempName = null;
+		
+		LocalDate startDate = null;
+		LocalDate endDate = null;
+		LocalTime startTime = null;
+		LocalTime endTime = null;
+	
+		while(nameParser.hasQuotations(process)){
+		tempName = nameParser.getNameWithinQuotationMarks(process);
+		if(tempName != ""){
+			if(!taskNameFlag){
+				taskName = tempName.replace("\"", "");
+			}
+			else{
+				edittedName = tempName.replace("\"", "");
+			}
+			process = process.replace(tempName, "");
+			process = process.trim();
+			System.out.println(taskName);
+			taskNameFlag = true;
+		}
+		}
+		String[] processArray = process.split("\\s+");
+
+
+		int j;
+
+		
+		for (int i = 0; i < processArray.length; i++) {
+			
+			if(i==0){
+				if(commandParser.isCommand(processArray[0])){
+				command = commandParser.getCommand(processArray[0]);
+				}
+				else {
+				command = "add";
+				}
+			}
+
+			
+			if (nameParser.checkNameFormat(processArray[i])) {
+				Queue<String> commandQueue = new LinkedList<String>();
+				for (j = i; j < processArray.length; j++) {
+					if (nameParser.checkNameFormat(processArray[j])) {
+						commandQueue.add(processArray[j]);
+					} else {
+						break;
+					}
+				}
+				if(!taskNameFlag){
+					taskName = NameExtractor.processName(commandQueue);
+				}
+				else {
+					edittedName = NameExtractor.processName(commandQueue);
+				}
+				taskNameFlag = true;
+				i=j-1;
+				System.out.println(taskName);
+			
+			}
+			else if (timeParser.checkTimeFormat(processArray[i])) {
+				if(!timeFlag){
+				startTime = timeParser.processTime(processArray[i]);
+				timeFlag = true;
+				}
+				else if(timeFlag){
+				endTime = timeParser.processTime(processArray[i]);
+				}
+				
+
+			} else if (dateParser.checkDateFormat(processArray[i])) {
+				Queue<String> commandQueue = new LinkedList<String>();
+				for (j = i; j < processArray.length; j++) {
+					if (dateParser.checkDateFormat(processArray[j])) {
+						commandQueue.add(processArray[j]);
+					} else {
+						break;
+					}
+				}
+				i = j - 1;
+				if(!dateFlag){
+				startDate = dateParser.processDate(commandQueue);
+				dateFlag = true;
+				}
+				else if(dateFlag){
+				endDate = dateParser.processDate(commandQueue);
+				}
+				
+
+			} else {
+				System.out.println("invalid command");
+			}
+
+		}
+
+		return setUpCommandObject(command, taskName, edittedName, startDate, startTime, endDate, endTime);
+
+	}
+
+	private static Command setUpCommandObject(String command, String taskName,
+			String edittedName, LocalDate startDate, LocalTime startTime,
+			LocalDate endDate, LocalTime endTime) {
+
+		Command object = new Command(command, taskName, edittedName, startDate,
+				startTime, endDate, endTime);
+		return object;
+	}
+}
---
+++ src/mhs/src/ConfigFile.java
@@ -0,0 +1,121 @@
+package mhs.src;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.joda.time.DateTime;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
+
+public class ConfigFile {
+
+	private Gson gson;
+	private JsonReader jsonReader;
+	private InputStream inputStream;
+	private File configFile;
+
+	private static String CONFIG_FILENAME;
+	private final static String DEFAULT_CONFIG_FILENAME = "configFile.json";
+
+	Map<String, String> configParameters;
+
+	public ConfigFile(String configFileName) throws IOException {
+		CONFIG_FILENAME = configFileName;
+		configParameters = new HashMap<String, String>();
+		initializeGson();
+		initializeConfigFile();
+	}
+
+	public ConfigFile() throws IOException {
+		CONFIG_FILENAME = DEFAULT_CONFIG_FILENAME;
+		configParameters = new HashMap<String, String>();
+		initializeGson();
+		initializeConfigFile();
+	}
+
+	private void initializeConfigFile() throws IOException {
+		configFile = new File(CONFIG_FILENAME);
+		if (!configFile.exists()) {
+			createNewJsonFile();
+		}
+		loadConfigFile();
+	}
+
+	private void createNewJsonFile() throws IOException {
+		configFile.createNewFile();
+		FileWriter fileWriter = new FileWriter(configFile);
+		fileWriter.write(gson.toJson(configParameters));
+		fileWriter.close();
+	}
+
+	private void initializeGson() {
+		GsonBuilder gsonBuilder = new GsonBuilder();
+		gsonBuilder.registerTypeAdapter(DateTime.class,
+				new DateTimeTypeConverter());
+		gsonBuilder.registerTypeAdapter(Task.class, new TaskTypeConverter());
+		gson = gsonBuilder.serializeNulls().create();
+	}
+
+	@SuppressWarnings("unchecked")
+	private void loadConfigFile() throws IOException {
+		inputStream = new FileInputStream(configFile);
+		jsonReader = new JsonReader(new InputStreamReader(inputStream, "UTF-8"));
+
+		JsonParser parser = new JsonParser();
+		JsonObject configJObject = parser.parse(jsonReader).getAsJsonObject();
+
+		configParameters = gson.fromJson(configJObject,
+				configParameters.getClass());
+
+		jsonReader.close();
+		inputStream.close();
+	}
+
+	public void save() throws IOException {
+		FileWriter fileWriter = new FileWriter(configFile);
+		fileWriter.write(gson.toJson(configParameters));
+		fileWriter.close();
+	}
+
+	public boolean hasConfigParameter(String parameter) {
+		if (configParameters.get(parameter) == null) {
+			return false;
+		}
+		return true;
+	}
+
+	/**
+	 * Get Config Parameter - userGoogleUserEmail - googleAuthToken
+	 * 
+	 * @param parameter
+	 * @return
+	 */
+	public String getConfigParameter(String parameter) {
+		return configParameters.get(parameter);
+	}
+
+	/**
+	 * Set config parameter
+	 * 
+	 * @param parameter
+	 * @param value
+	 * @return
+	 * @throws IOException
+	 */
+	public void setConfigParameter(String parameter, String value)
+			throws IOException {
+		configParameters.put(parameter, value);
+		save();
+	}
+}
