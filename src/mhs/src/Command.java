8572daa97e217f10eb290a9405c711694686663f
c_kahou <c_kahou@hotmail.com>
2012-10-07 03:56:38 +0800

    refactored code , added comments, removed date class, added today/tomorrow and rectification of invalid dates.

--- src/mhs/src/Command.java
+++ src/mhs/src/Command.java
@@ -3,9 +3,20 @@ package mhs.src;
 import org.joda.time.DateTime;
 import org.joda.time.LocalDate;
 import org.joda.time.LocalTime;
+/**
+ * 
+ * @author Cheong Kahou
+ * A0086805X
+ */
 
+/**
+ * This is the class to package the parameters into a command object. 
+ */
 public class Command {
 
+	/**
+	 * This is the enum of the different type of commands. 
+	 */
 	public enum command{
 		add, remove,edit, search, sync,undo;
 	}
@@ -17,6 +28,17 @@ public class Command {
 	private DateTime startDate;
 	private DateTime endDate;
 
+	/**
+	 * This is the constructor to set up the command object. 
+	 * 
+	 * @param commandInput This is the command.
+	 * @param taskNameInput This is the name of the task.
+	 * @param edittedNameInput This is the editted name of the task.
+	 * @param startDateInput This is the start date.
+	 * @param startTimeInput This is the start time.
+	 * @param endDateInput This is the end date. 
+	 * @param endTimeInput This is the end time. 
+	 */
 	public Command(String commandInput, String taskNameInput,
 			String edittedNameInput, LocalDate startDateInput,
 			LocalTime startTimeInput, LocalDate endDateInput,
@@ -58,9 +80,20 @@ public class Command {
 		if(endDateInput !=null && endTimeInput==null){
 			endDate = endDateInput.toDateTimeAtStartOfDay();
 		}
-
+		
+		if(startDate != null && endDate != null){
+			if(startDate.isAfter(endDate)){
+			DateTime temp = new DateTime();
+			temp = startDate; 
+			endDate = startDate; 
+			startDate = temp;
+			}
+		}
 	}
 
+	/**
+	 * Default constructor setting all parameters to null.
+	 */
 	public Command() {
 		commandEnum = null;
 		taskName = null;
@@ -69,45 +102,45 @@ public class Command {
 		edittedName = null;
 	}
 
+	/**
+	 * Getter for task name.
+	 * @return Returns the task name. 
+	 */
 	public String getTaskName() {
 		return taskName;
 	}
 
-	public void setTaskName(String taskName) {
-		this.taskName = taskName;
-	}
-
+	/**
+	 * Getter for editted name.
+	 * @return Returns the editeed name. 
+	 */
 	public String getEdittedName() {
 		return edittedName;
 	}
 
-	public void setEdittedName(String edittedName) {
-		this.edittedName = edittedName;
-	}
-
+	/**
+	 * Getter for end date.
+	 * @return Returns the end date. 
+	 */
 	public DateTime getEndDate() {
 		return endDate;
 	}
 
-	public void setEndDate(DateTime endDate) {
-		this.endDate = endDate;
-	}
-
+	/**
+	 * Getter for start date.
+	 * @return Returns the start date. 
+	 */
 	public DateTime getStartDate() {
 		return startDate;
 	}
 
-	public void setStartDate(DateTime startDate) {
-		this.startDate = startDate;
-	}
-
+	/**
+	 * Getter for the command.
+	 * @return Returns the command. 
+	 */
 	public command getCommandEnum() {
 		return commandEnum;
 	}
 
-	public void setCommandEnum(command commandEnum) {
-		this.commandEnum = commandEnum;
-	}
-	
 	
 }
--- src/mhs/src/CommandExtractor.java
+++ src/mhs/src/CommandExtractor.java
@@ -1,11 +1,27 @@
 package mhs.src;
 
+/**
+ * 
+ * @author Cheong Kahou
+ *A0086805X
+ *
+ */
+
+/**
+ * 
+ * This is a class that checks string if they are commands and set the commands. 
+ *
+ */
 public class CommandExtractor {
 
+	/**
+	 * 
+	 *These are the enum commands that are used and the different keywords the user may enter.
+	 */
 	enum commands {
 		add("add"), remove("remove"), delete("remove"), update("edit"), edit(
-				"edit"), postpone("edit"), search("search"), find("search"), display("search"), sync(
-				"sync"), undo("undo");
+				"edit"), postpone("edit"), search("search"), find("search"), display(
+				"search"), sync("sync"), undo("undo");
 
 		private final String command;
 
@@ -17,23 +33,35 @@ public class CommandExtractor {
 
 	private String commandString;
 
-	public boolean isCommand(String printString){
+	/**
+	 * This is a function to check if a string is a command.
+	 * 
+	 * @param printString This is the string to be checked.
+	 * 
+	 * @return Returns if the string is a command type. 
+	 */
+	public boolean isCommand(String printString) {
 		for (commands c : commands.values()) {
 			if (printString.equalsIgnoreCase(c.name())) {
 				return true;
 			}
 		}
-			return false;
+		return false;
 	}
-		
-	public String getCommand(String printString) {
+
+	/**
+	 * This is the function to set the command. 
+	 * 
+	 * @param printString This the command String to be set.
+	 * 
+	 * @return Returns the command that is set. 
+	 */
+	public String setCommand(String printString) {
 		for (commands c : commands.values()) {
 			if (printString.equalsIgnoreCase(c.name())) {
 				commandString = c.command;
-				return commandString;
 			}
 		}
-		commandString = "add";
 		return commandString;
 	}
 }
