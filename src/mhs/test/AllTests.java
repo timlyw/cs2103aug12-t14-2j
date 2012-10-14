db25603694cf96f1b2935bd4826f2dfb66032f91
timlyw <timlyw@localhost>
2012-10-14 20:58:46 +0800

    Updated AllTests

--- src/mhs/test/AllTests.java
+++ src/mhs/test/AllTests.java
@@ -5,7 +5,8 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 @RunWith(Suite.class)
-@SuiteClasses({ DatabaseTest.class, ConfigFileTest.class, TaskRecordFileTest.class })
+@SuiteClasses({ DatabaseTest.class, ConfigFileTest.class,
+		GoogleCalendarTest.class, TaskRecordFileTest.class })
 public class AllTests {
 
 }
