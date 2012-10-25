244ede1d8526b1b7fb55a5d3d1d09ba41be5f2cf
c_kahou <c_kahou@hotmail.com>
2012-10-26 01:03:58 +0800

    Merge with 1ef5d6a2aa4ab6942e25fb3ee120f26cfde9162d

--- src/mhs/test/AllTests.java
+++ src/mhs/test/AllTests.java
@@ -7,7 +7,7 @@ import org.junit.runners.Suite.SuiteClasses;
 @RunWith(Suite.class)
 @SuiteClasses({ DatabaseTest.class, ConfigFileTest.class,
 		GoogleCalendarTest.class, TaskRecordFileTest.class,
-		CommandParserTest.class })
+		CommandParserTest.class, TaskListsTest.class})
 public class AllTests {
 
 }
