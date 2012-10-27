07177eebefa8d47dfc96cf14ef458e5387c8af8c
Shekhar Baggavalli <brshekhar1@gmail.com>
2012-10-28 00:51:30 +0800

    added core func Command classes and subclasses

--- src/mhs/src/logic/CommandRemove.java
+++ src/mhs/src/logic/CommandRemove.java
@@ -1,5 +1,75 @@
 package mhs.src.logic;
 
+import java.io.IOException;
+import java.util.List;
+
+import mhs.src.storage.Task;
+
 public class CommandRemove extends Command {
+
+	private Task lastDeletedTask;
+
+	public CommandRemove(CommandInfo userCommand) {
+		List<Task> resultList;
+		try {
+			resultList = queryTask(userCommand);
+			matchedTasks = resultList;
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	@Override
+	public String executeCommand() {
+		String outputString = new String();
+
+		if (matchedTasks.isEmpty()) {
+			outputString = "No matching results found";
+		}
+		// if only 1 match is found then display it
+		else if (matchedTasks.size() == 1) {
+			try {
+				lastDeletedTask = new Task();
+				lastDeletedTask = matchedTasks.get(0);
+				dataHandler.delete(matchedTasks.get(0).getTaskId());
+				isUndoable = true;
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			outputString = "Deleted Task - '"
+					+ matchedTasks.get(0).getTaskName() + "'";
+		}
+		// if multiple matches are found display the list
+		else {
+			outputString = displayListOfTasks(matchedTasks);
+		}
+		return outputString;
+	}
+
+	@Override
+	public String undo() {
+		if (isUndoable()) {
+			try {
+				dataHandler.add(lastDeletedTask);
+				isUndoable = false;
+				return MESSAGE_UNDO_CONFIRM;
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+				return MESSAGE_UNDO_FAIL;
+			}
+
+		} else {
+			return MESSAGE_UNDO_FAIL;
+		}
+	}
+
+	@Override
+	public String executeByIndex(int index) {
+		// TODO Auto-generated method stub
+		return null;
+	}
 
 }
