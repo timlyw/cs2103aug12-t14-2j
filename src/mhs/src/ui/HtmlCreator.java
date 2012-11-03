6a6f247825ba12e8b995ca9696b895ee0c822b30
jwwwwong <jwwwwong@localhost>
2012-11-03 23:09:23 +0800

    added area for MHS title, implemented createTaskListHtml in HtmlCreator

--- src/mhs/src/ui/HtmlCreator.java
+++ src/mhs/src/ui/HtmlCreator.java
@@ -1,15 +1,28 @@
 package mhs.src.ui;
 
+import java.util.List;
+
+import mhs.src.storage.Task;
+import mhs.src.storage.TaskCategory;
+
+import org.joda.time.DateTime;
+
 public class HtmlCreator {
 	private static final String HTML_FEEDBACK = "<html><font color='#383838' size='5' face='calibri'>%1$s</font></html>";
 	private static final String HTML_DISPLAY = "<html><font size='5' face='calibri'>%1$s</font></html>";
 	private static final String HTML_TITLE = "<html><center><font size='5' face='calibri'>%1$s</font></center></html>";
 	
-	public static final String BLUE = "#383838";
+	public static final String BLUE = "#3300CC";
 	public static final String PURPLE = "#660066";
+	public static final String ORANGE = "#FF6600";
+	public static final String LIGHT_BLUE = "#3333FF";
+	public static final String LIGHT_GRAY = "#B8B8B8";
+	
 	
 	private static final String FORMAT_BOLD = "<b>%1$s</b>";
 	private static final String FORMAT_COLOR = "<font color=%1$s>%2$s</font>";
+	
+	Task lastTask = null;
 	
 	public final String NEW_LINE = "<br/>";
 	
@@ -37,4 +50,90 @@ public class HtmlCreator {
 		String boldString = String.format(FORMAT_COLOR, color, htmlText);
 		return boldString;
 	}
+	
+	public Task getLastTaskPrinted() {
+		return lastTask;
+	}
+	
+	public String createTaskListHtml(List<Task> taskList, int lineLimit) {
+		String taskListHtml = "";
+		
+		DateTime prevTaskDateTime = null;
+		
+		int lineCount = 0;
+		
+		for(int i = 0; i < taskList.size() && lineCount < lineLimit; i++) {
+			Task task = taskList.get(i);
+			DateTime currTaskDateTime = null;
+			
+			if(isTimed(task)) {
+				currTaskDateTime = task.getStartDateTime();
+			} else if(isDeadline(task)) {
+				currTaskDateTime = task.getEndDateTime();
+			} else if(isFloating(task)) {
+				if(i == 0) {
+					taskListHtml += color("Floating Tasks:", LIGHT_BLUE) + NEW_LINE;
+					lineCount += 2;
+				}
+				
+				currTaskDateTime = null;
+			} else {
+				continue;
+			}
+			
+			if(!dateIsEqual(prevTaskDateTime, currTaskDateTime) && currTaskDateTime != null) {
+				if(i > 0) {
+					taskListHtml += NEW_LINE;
+					lineCount++;
+				}
+				String dateString = getDateString(currTaskDateTime);
+				dateString = color(dateString, BLUE);
+				taskListHtml +=  dateString + NEW_LINE;
+			}
+			
+			prevTaskDateTime = currTaskDateTime;
+			taskListHtml += Integer.toString(i + 1) + ". " + task.toHtmlString() + NEW_LINE;
+			lineCount += 2;
+			lastTask = task;
+		}
+		
+		return taskListHtml;
+	}
+	
+	public boolean isTimed(Task task) {
+		if(task.getTaskCategory() == TaskCategory.TIMED) {
+			return true;
+		} 
+		return false;
+	}
+	
+	public boolean isDeadline(Task task) {
+		if(task.getTaskCategory() == TaskCategory.DEADLINE) {
+			return true;
+		} 
+		return false;
+	}
+	
+	public boolean isFloating(Task task) {
+		if(task.getTaskCategory() == TaskCategory.FLOATING) {
+			return true;
+		} 
+		return false;
+	}
+	
+	private boolean dateIsEqual(DateTime date1, DateTime date2) {
+		if(date1 == null || date2 == null) {
+			return false;
+		}
+		
+		if(date1.getDayOfYear() == date2.getDayOfYear() && date1.getYear() == date2.getYear()) {
+			return true;
+		}
+		return false;
+	}
+	
+	private String getDateString(DateTime date) {
+		return date.toString("dd MMM yy");
+	}
+	
 }
