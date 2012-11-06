52e1af577b02c4cef42531f93570835b5cb6ac31
jwwwwong <jwwwwong@localhost>
2012-11-06 15:58:22 +0800

    fixed pagination

---
+++ src/mhs/src/common/exceptions/DatabaseAlreadyInstantiatedException.java
@@ -0,0 +1,33 @@
+//@author A0087048X
+
+package mhs.src.common.exceptions;
+
+/**
+ * DatabaseAlreadyInstantiatedException
+ * 
+ * Custom Exception thrown when instantiating new database factory when
+ * singleton database is already instantiated.
+ * 
+ * @author Timothy Lim Yi Wen A0087048X
+ * 
+ */
+
+@SuppressWarnings("serial")
+public class DatabaseAlreadyInstantiatedException extends Exception {
+
+	public DatabaseAlreadyInstantiatedException() {
+	}
+
+	public DatabaseAlreadyInstantiatedException(String message) {
+		super(message);
+	}
+
+	public DatabaseAlreadyInstantiatedException(Throwable cause) {
+		super(cause);
+	}
+
+	public DatabaseAlreadyInstantiatedException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
---
+++ src/mhs/src/common/exceptions/TaskNotFoundException.java
@@ -0,0 +1,28 @@
+//@author A0087048X
+package mhs.src.common.exceptions;
+
+/**
+ * TaskNotFoundException
+ * 
+ * Custom Exception thrown when task specified by taskId is not found
+ * 
+ * @author Timothy Lim Yi Wen A0087048X
+ */
+@SuppressWarnings("serial")
+public class TaskNotFoundException extends Exception {
+
+	public TaskNotFoundException() {
+	}
+
+	public TaskNotFoundException(String message) {
+		super(message);
+	}
+
+	public TaskNotFoundException(Throwable cause) {
+		super(cause);
+	}
+
+	public TaskNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+}
