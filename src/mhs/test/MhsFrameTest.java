0c832e0238ffad5e06b4a4ecc0638894f9af7a87
jwwwwong <jwwwwong@localhost>
2012-11-08 09:00:36 +0800

    updated test comments

---
+++ src/mhs/test/MhsFrameTest.java
@@ -0,0 +1,26 @@
+package mhs.test;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
+import mhs.src.userinterface.MhsFrame;
+
+public class MhsFrameTest {
+	
+	/**
+	 * basic component initialization test
+	 * tests if the various components of MhsFrame are properly added
+	 * to the interface
+	 */
+	@Test
+	public void testMhsFrameInitialization() {
+		MhsFrame mhsFrame = MhsFrame.getInstance();
+		assertTrue(mhsFrame.framePanelInitialized());
+		assertTrue(mhsFrame.titleScreenInitialized());
+		assertTrue(mhsFrame.displayScreenInitialized());
+		assertTrue(mhsFrame.feedbackScreenInitialized());
+		assertTrue(mhsFrame.plainTextBoxInitialized());
+		assertTrue(mhsFrame.passwordBoxInitialized());
+	}
+}
