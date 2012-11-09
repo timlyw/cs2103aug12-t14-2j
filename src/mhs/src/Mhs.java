ab72917976960c1012700558027d4ff19324ad3e
timlyw <timlyw@localhost>
2012-11-10 01:15:19 +0800

    Updated functional MhsGoogleOAuth2 class (authentication/refresh mechanism) and refactored MhsGoogleOAuth2

--- src/mhs/src/Mhs.java
+++ src/mhs/src/Mhs.java
@@ -19,21 +19,6 @@ import mhs.src.userinterface.UiController;
 
 public class Mhs {
 	public static void main(String[] args) {
-
-		URL remoteStorageServiceUrl;
-		try {
-			remoteStorageServiceUrl = new URL(
-					"https://accounts.google.com/o/oauth2/auth?scope=https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.email+https%3A%2F%2Fwww.googleapis.com%2Fauth%2Fuserinfo.profile&redirect_uri=urn:ietf:wg:oauth:2.0:oob&response_type=code&client_id=812741506391-h38jh0j4fv0ce1krdkiq0hfvt6n5amrf.apps.googleusercontent.com");
-			URLConnection googleAuthConnection = remoteStorageServiceUrl
-					.openConnection();
-		} catch (MalformedURLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-
 		UiController uiController = new UiController();
 		uiController.showUserInterface();
 	}
