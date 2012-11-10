4ff4cc607908a47135223d38ab75d7e93e4651a2
timlyw <timlyw@localhost>
2012-11-10 20:58:53 +0800

    Update Sync, Changed use of getAuthenticatedUserGoogleAccountName  in processor

--- src/mhs/src/storage/persistence/remote/GoogleCalendarMhs.java
+++ src/mhs/src/storage/persistence/remote/GoogleCalendarMhs.java
@@ -9,6 +9,7 @@ import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.services.calendar.model.Event;
+import com.google.gdata.util.ResourceNotFoundException;
 
 
 public class GoogleCalendarMhs {
@@ -40,7 +41,7 @@ public class GoogleCalendarMhs {
 		return createdEvent;
 	}
 	
-	public Event retrieveEvent(String eventId) throws IOException {
+	public Event retrieveEvent(String eventId) throws IOException, ResourceNotFoundException {
 		Event retrievedEvent = null;
 		retrievedEvent = defaultCalendar.retrieveEvent(eventId);
 		if(retrievedEvent == null) {
@@ -50,7 +51,7 @@ public class GoogleCalendarMhs {
 		return retrievedEvent;
 	}
 	
-	public List<Event> retrieveEvents(String minDate, String maxDate) throws IOException {
+	public List<Event> retrieveEvents(String minDate, String maxDate) throws IOException, ResourceNotFoundException {
 		List<Event> defaultList = defaultCalendar.retrieveEvents(minDate, maxDate);
 		List<Event> completedList = completedCalendar.retrieveEvents(minDate, maxDate);
 		defaultList.addAll(completedList);
@@ -58,7 +59,7 @@ public class GoogleCalendarMhs {
 		return defaultList;
 	}
 	
-	public Event updateEvent(Task updatedTask) throws IOException {
+	public Event updateEvent(Task updatedTask) throws IOException, ResourceNotFoundException {
 		String eventId = updatedTask.getgCalTaskId();
 		String title = updatedTask.getTaskName();
 		String startTime = updatedTask.getStartDateTime().toString();
@@ -68,7 +69,7 @@ public class GoogleCalendarMhs {
 		return updatedEvent;
 	}
 	
-	public void deleteEvent(String eventId) throws IOException {
+	public void deleteEvent(String eventId) throws IOException, ResourceNotFoundException {
 		defaultCalendar.deleteEvent(eventId);
 		completedCalendar.deleteEvent(eventId);
 	}
@@ -77,7 +78,7 @@ public class GoogleCalendarMhs {
 		return defaultCalendar.isDeleted(event);
 	}
 	
-	public void deleteEvents(String startTime, String endTime) throws IOException {
+	public void deleteEvents(String startTime, String endTime) throws IOException, ResourceNotFoundException {
 		defaultCalendar.deleteEvents(startTime, endTime);
 		completedCalendar.deleteEvents(startTime, endTime);
 	}
