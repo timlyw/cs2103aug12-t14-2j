0786cabe5f79aa25bdd33b67e9725c8c373543f4
c_kahou <c_kahou@hotmail.com>
2012-11-10 15:04:36 +0800

    Added GoogleTasks

---
+++ src/mhs/src/storage/persistence/remote/GoogleTasks.java
@@ -0,0 +1,72 @@
+package mhs.src.storage.persistence.remote;
+
+
+import java.io.IOException;
+import java.util.List;
+
+import com.google.api.client.http.HttpRequestInitializer;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.services.tasks.Tasks;
+import com.google.api.services.tasks.model.Task;
+
+
+public class GoogleTasks {
+	String DEFAULT_TASK_LIST_ID = "@default";
+	String STATUS_COMPLETED = "completed";
+	String STATUS_NEEDS_ACTION = "needsAction";
+	
+	Tasks taskService = null;
+	String taskListId = null;
+	
+	public GoogleTasks(HttpTransport httpTransport, JsonFactory jsonFactory, HttpRequestInitializer httpRequestInitializer) {
+		initTaskService(httpTransport, jsonFactory, httpRequestInitializer);
+		initTaskListId();
+	}
+	
+	public void initTaskService(HttpTransport httpTransport, JsonFactory jsonFactory, HttpRequestInitializer httpRequestInitializer) {
+		taskService = new Tasks(httpTransport, jsonFactory, httpRequestInitializer);
+	}
+	
+	public void initTaskListId() {
+		taskListId = DEFAULT_TASK_LIST_ID;
+	}
+	
+	public List<Task> retrieveTasks() throws IOException {
+		com.google.api.services.tasks.model.Tasks retrievedTaskList = taskService.tasks().list(taskListId).execute();
+		return retrievedTaskList.getItems();
+	}
+	
+	public Task createTask(String title, boolean completed) throws IOException {
+		Task task = new Task();
+		task.setTitle(title);
+		setCompleted(task, completed);
+		Task createdTask = taskService.tasks().insert(taskListId, task).execute();
+		return createdTask;
+	}
+
+	public void updateTask(String taskId, String title, boolean completed) throws IOException {
+		Task taskToBeUpdated = retrieveTask(taskId);
+		taskToBeUpdated.setTitle(title);
+		setCompleted(taskToBeUpdated, completed);
+		taskService.tasks().update(taskListId, taskId, taskToBeUpdated).execute();
+	}
+	
+	public Task retrieveTask(String taskId) throws IOException {
+		Task task = taskService.tasks().get(taskListId, taskId).execute();
+		return task;
+	}
+	
+	public void deleteTask(String taskId) throws IOException {
+		
+		taskService.tasks().delete(taskListId, taskId).execute();
+	}
+	
+	private void setCompleted(Task task, boolean completed) {
+		if(completed) {
+			task.setStatus(STATUS_COMPLETED);
+		} else {
+			task.setStatus(STATUS_NEEDS_ACTION);
+		}
+	}
+}
