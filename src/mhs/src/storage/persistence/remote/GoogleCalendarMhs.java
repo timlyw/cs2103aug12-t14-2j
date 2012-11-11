86006b1d5759483025159da75b4752c1711a4a7a
jwwwwong <jwwwwong@localhost>
2012-11-11 14:27:50 +0800

    refactored processor

--- src/mhs/src/storage/persistence/remote/GoogleCalendarMhs.java
+++ src/mhs/src/storage/persistence/remote/GoogleCalendarMhs.java
@@ -9,78 +9,88 @@ import com.google.api.client.http.HttpRequestInitializer;
 import com.google.api.client.http.HttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.services.calendar.model.Event;
-
+import com.google.gdata.util.ResourceNotFoundException;
 
 public class GoogleCalendarMhs {
-	private static final String COMPLETED_TASKS_CALENDAR_TITLE = "Completed Tasks (MHS)";
-	private GoogleCalendar defaultCalendar;
-	private GoogleCalendar completedCalendar;
-	
-	public GoogleCalendarMhs(HttpTransport httpTransport, JsonFactory jsonFactory, HttpRequestInitializer httpRequestInitializer) throws IOException {
-		defaultCalendar = new GoogleCalendar(httpTransport, jsonFactory, httpRequestInitializer);
-		String completedCalendarId = defaultCalendar.createCalendar(COMPLETED_TASKS_CALENDAR_TITLE);
-		completedCalendar = new GoogleCalendar(httpTransport, jsonFactory, httpRequestInitializer);
-		completedCalendar.setCalendarId(completedCalendarId);
-	}
-	
-	public Event createEvent(Task newTask) throws IOException {
-		if(newTask.isFloating()) {
-			return null;
-		}
-		String title = newTask.getTaskName();
-		String startTime = newTask.getStartDateTime().toString();
-		String endTime = newTask.getEndDateTime().toString();
-		Event createdEvent = null;
-		if(newTask.isDone()) {
-			completedCalendar.createEvent(title, startTime, endTime);
-		} else {
-			defaultCalendar.createEvent(title, startTime, endTime);
-		}
-		
-		return createdEvent;
-	}
-	
-	public Event retrieveEvent(String eventId) throws IOException {
-		Event retrievedEvent = null;
-		retrievedEvent = defaultCalendar.retrieveEvent(eventId);
-		if(retrievedEvent == null) {
-			retrievedEvent = completedCalendar.retrieveEvent(eventId);
-		} 
-		
-		return retrievedEvent;
-	}
-	
-	public List<Event> retrieveDefaultEvents(String minDate, String maxDate) throws IOException {
-		return defaultCalendar.retrieveEvents(minDate, maxDate);
-	}
+        private static final String COMPLETED_TASKS_CALENDAR_TITLE = "Completed Tasks (MHS)";
+        private GoogleCalendar defaultCalendar;
+        private GoogleCalendar completedCalendar;
 
-	public List<Event> retrieveCompletedEvents(String minDate, String maxDate) throws IOException {
-		return completedCalendar.retrieveEvents(minDate, maxDate);		
-	}
-	
-	public Event updateEvent(Task updatedTask) throws IOException {
-		String eventId = updatedTask.getgCalTaskId();
-		
-		deleteEvent(eventId);
-		return createEvent(updatedTask);
-	}
-	
-	
-	public void deleteEvent(String eventId) throws IOException {
-		defaultCalendar.deleteEvent(eventId);
-		completedCalendar.deleteEvent(eventId);
-	}
-	
-	public boolean isDeleted(Event event) {
-		return defaultCalendar.isDeleted(event);
-	}
-	
-	public void deleteEvents(String startTime, String endTime) throws IOException {
-		defaultCalendar.deleteEvents(startTime, endTime);
-		completedCalendar.deleteEvents(startTime, endTime);
-	}
-	
-	public void isEventCompleted() {
-		
-	}
+        public GoogleCalendarMhs(HttpTransport httpTransport,
+                        JsonFactory jsonFactory,
+                        HttpRequestInitializer httpRequestInitializer) throws IOException {
+                defaultCalendar = new GoogleCalendar(httpTransport, jsonFactory,
+                                httpRequestInitializer);
+                String completedCalendarId = defaultCalendar
+                                .createCalendar(COMPLETED_TASKS_CALENDAR_TITLE);
+                completedCalendar = new GoogleCalendar(httpTransport, jsonFactory,
+                                httpRequestInitializer);
+                completedCalendar.setCalendarId(completedCalendarId);
+        }
+
+        public Event createEvent(Task newTask) throws IOException {
+                if (newTask.isFloating()) {
+                        return null;
+                }
+                String title = newTask.getTaskName();
+                String startTime = newTask.getStartDateTime().toString();
+                String endTime = newTask.getEndDateTime().toString();
+                Event createdEvent = null;
+                if (newTask.isDone()) {
+                        completedCalendar.createEvent(title, startTime, endTime);
+                } else {
+                        defaultCalendar.createEvent(title, startTime, endTime);
+                }
+
+                return createdEvent;
+        }
+
+        public Event retrieveEvent(String eventId) throws IOException,
+                        ResourceNotFoundException {
+                Event retrievedEvent = null;
+                retrievedEvent = defaultCalendar.retrieveEvent(eventId);
+                if (retrievedEvent == null) {
+                        retrievedEvent = completedCalendar.retrieveEvent(eventId);
+                }
+
+                return retrievedEvent;
+        }
+
+        public List<Event> retrieveDefaultEvents(String minDate, String maxDate)
+                        throws IOException, ResourceNotFoundException {
+                return defaultCalendar.retrieveEvents(minDate, maxDate);
+        }
+
+        public List<Event> retrieveCompletedEvents(String minDate, String maxDate)
+                        throws IOException, ResourceNotFoundException {
+                return completedCalendar.retrieveEvents(minDate, maxDate);
+        }
+
+        public Event updateEvent(Task updatedTask) throws IOException,
+                        ResourceNotFoundException {
+                String eventId = updatedTask.getgCalTaskId();
+
+                deleteEvent(eventId);
+                return createEvent(updatedTask);
+        }
+
+        public void deleteEvent(String eventId) throws IOException,
+                        ResourceNotFoundException {
+                defaultCalendar.deleteEvent(eventId);
+                completedCalendar.deleteEvent(eventId);
+        }
+
+        public boolean isDeleted(Event event) {
+                return defaultCalendar.isDeleted(event);
+        }
+
+        public void deleteEvents(String startTime, String endTime)
+                        throws IOException, ResourceNotFoundException {
+                defaultCalendar.deleteEvents(startTime, endTime);
+                completedCalendar.deleteEvents(startTime, endTime);
+        }
+
+        public void isEventCompleted() {
+
+        }
 }
--- src/mhs/src/storage/persistence/task/FloatingTask.java
+++ src/mhs/src/storage/persistence/task/FloatingTask.java
@@ -17,66 +17,66 @@ import org.joda.time.DateTime;
  */
 public class FloatingTask extends Task {
 
-	private String gTaskId;
+        private String gTaskId;
 
-	/**
-	 * Constructor with TaskCategory taskCategory
-	 * 
-	 * @param taskId
-	 * @param taskName
-	 * @param taskCategory
-	 * @param createdDt
-	 * @param updatedDt
-	 * @param syncDt
-	 * @param isDone
-	 * @param isDeleted
-	 */
-	public FloatingTask(int taskId, String taskName, TaskCategory taskCategory,
-			DateTime createdDt, DateTime updatedDt, DateTime syncDt,
-			String gTaskId, boolean isDone, boolean isDeleted) {
-		super(taskId, taskName, taskCategory, createdDt, updatedDt, syncDt,
-				isDone, isDeleted);
-		this.setGTaskId(gTaskId);
-	}
+        /**
+         * Constructor with TaskCategory taskCategory
+         * 
+         * @param taskId
+         * @param taskName
+         * @param taskCategory
+         * @param createdDt
+         * @param updatedDt
+         * @param syncDt
+         * @param isDone
+         * @param isDeleted
+         */
+        public FloatingTask(int taskId, String taskName, TaskCategory taskCategory,
+                        DateTime createdDt, DateTime updatedDt, DateTime syncDt,
+                        String gTaskId, boolean isDone, boolean isDeleted) {
+                super(taskId, taskName, taskCategory, createdDt, updatedDt, syncDt,
+                                isDone, isDeleted);
+                this.setGTaskId(gTaskId);
+        }
 
-	/**
-	 * Construct synced floating task with google task 
-	 * 
-	 * @param taskId
-	 * @param googleTask
-	 * @param syncDateTime
-	 */
-	public FloatingTask(int taskId,
-			com.google.api.services.tasks.model.Task googleTask,
-			DateTime syncDateTime) {
-		super(taskId, googleTask.getTitle(), TaskCategory.FLOATING,
-				syncDateTime, syncDateTime, syncDateTime, false, false);
-		setGTaskId(googleTask.getId());
-	}
+        /**
+         * Construct synced floating task with google task 
+         * 
+         * @param taskId
+         * @param googleTask
+         * @param syncDateTime
+         */
+        public FloatingTask(int taskId,
+                        com.google.api.services.tasks.model.Task googleTask,
+                        DateTime syncDateTime) {
+                super(taskId, googleTask.getTitle(), TaskCategory.FLOATING,
+                                syncDateTime, syncDateTime, syncDateTime, false, false);
+                setGTaskId(googleTask.getId());
+        }
 
-	public String getGTaskId() {
-		return gTaskId;
-	}
+        public String getGTaskId() {
+                return gTaskId;
+        }
 
-	public void setGTaskId(String gTaskId) {
-		this.gTaskId = gTaskId;
-	}
+        public void setGTaskId(String gTaskId) {
+                this.gTaskId = gTaskId;
+        }
 
-	/**
-	 * @author John Wong
-	 */
-	public String toHtmlString() {
-		HtmlCreator htmlCreator = new HtmlCreator();
+        /**
+         * @author John Wong
+         */
+        public String toHtmlString() {
+                HtmlCreator htmlCreator = new HtmlCreator();
 
-		String boldTaskName = taskName;
-		String htmlString = boldTaskName;
+                String boldTaskName = taskName;
+                String htmlString = boldTaskName;
 
-		if (isDone()) {
-			htmlString = htmlCreator.color(taskName + " [completed]",
-					HtmlCreator.LIGHT_GRAY);
-		}
+                if (isDone()) {
+                        htmlString = htmlCreator.color(taskName + " [completed]",
+                                        HtmlCreator.LIGHT_GRAY);
+                }
 
-		return htmlString;
-	}
+                return htmlString;
+        }
 
 }
