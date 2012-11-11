f311f822c5f04ef7b88d54d46782d2a0245680df
c_kahou <c_kahou@hotmail.com>
2012-11-11 23:37:26 +0800

    made sub packages for logic class

---
+++ src/mhs/src/common/exceptions/NoActiveCredentialException.java
@@ -0,0 +1,30 @@
+//@author A0087048X
+
+package mhs.src.common.exceptions;
+
+/**
+ * NoActiveCredentialException
+ * 
+ * Custom Exception thrown no credential exists.
+ * 
+ * @author Timothy Lim Yi Wen A0087048X
+ */
+
+public class NoActiveCredentialException extends Exception {
+
+	public NoActiveCredentialException() {
+	}
+
+	public NoActiveCredentialException(String message) {
+		super(message);
+	}
+
+	public NoActiveCredentialException(Throwable cause) {
+		super(cause);
+	}
+
+	public NoActiveCredentialException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+}
---
+++ src/mhs/test/AllTests.java
@@ -0,0 +1,22 @@
+package mhs.test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * AllTests
+ * 
+ * jUnit Test Suite running all tests
+ * 
+ * - Add jUnit test to SuiteClasses include in AllTests
+ * 
+ */
+
+@RunWith(Suite.class)
+@SuiteClasses({ DatabaseFactoryTest.class, DatabaseTest.class,
+		ConfigFileTest.class, TaskRecordFileTest.class,
+		CommandParserTest.class, TaskListsTest.class })
+public class AllTests {
+
+}
