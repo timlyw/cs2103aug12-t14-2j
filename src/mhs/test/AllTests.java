6296d6cabd3eff7654778933895a832126fee53b
timlyw <timlyw@localhost>
2012-11-11 23:29:04 +0800

    Updated Database tests

--- src/mhs/test/AllTests.java
+++ src/mhs/test/AllTests.java
@@ -16,7 +16,7 @@ import org.junit.runners.Suite.SuiteClasses;
  */
 
 @RunWith(Suite.class)
-@SuiteClasses({ DatabaseFactoryTest.class, DatabaseTest.class,
+@SuiteClasses({ DatabaseFactoryTest.class, DatabaseSyncTest.class, DatabaseTest.class,
 		ConfigFileTest.class, TaskRecordFileTest.class,
 		CommandParserTest.class, TaskListsTest.class })
 public class AllTests {
