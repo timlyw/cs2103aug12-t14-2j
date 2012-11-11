f2d9cbcfce549156ca25f689b5498e8bcb29cfac
jwwwwong <jwwwwong@localhost>
2012-11-11 22:27:18 +0800

    removed .properties files

---
+++ src/mhs/test/AllTests.java
@@ -0,0 +1,24 @@
+package mhs.test;
+
+import mhs.src.storage.persistence.remote.GoogleCalendarMhs;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * AllTests
+ * 
+ * jUnit Test Suite running all tests
+ * 
+ * - Add jUnit test to SuiteClasses include in AllTests
+ * 
+ */
+
+@RunWith(Suite.class)
+@SuiteClasses({ DatabaseFactoryTest.class, DatabaseTest.class,
+		ConfigFileTest.class, TaskRecordFileTest.class,
+		CommandParserTest.class, TaskListsTest.class })
+public class AllTests {
+
+}
