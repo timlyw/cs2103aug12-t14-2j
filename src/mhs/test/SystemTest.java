bd37faefc4a80144d51198e275e2fb34be02d652
timlyw <timlyw@localhost>
2012-11-13 00:08:58 +0800

    Updated System Test

--- src/mhs/test/SystemTest.java
+++ src/mhs/test/SystemTest.java
@@ -24,23 +24,7 @@ public class SystemTest {
 	private static final String TEST_DATABASE_JSON = "testDatabase.json";
 	private Processor processor;
 
-	@BeforeClass
-	public static void setUpBeforeClass() throws Exception {
-	}
-
-	@AfterClass
-	public static void tearDownAfterClass() throws Exception {
-	}
-
 	@Before
-	public void setUp() throws Exception {
-	}
-
-	@After
-	public void tearDown() throws Exception {
-	}
-
-	@Test
 	/**
 	 * Sets up environment and starts System tests
 	 */
@@ -50,7 +34,6 @@ public class SystemTest {
 		processor.clearDatabase();
 		initializeLists();
 		processor.setLineLimit(20);
-		startTests();
 	}
 
 	/**
@@ -64,7 +47,8 @@ public class SystemTest {
 	/**
 	 * Starts executing all commands in inputfile.txt
 	 */
-	private void startTests() {
+	@Test
+	public void startTests() {
 
 		BufferedReader readFile;
 		try {
@@ -76,8 +60,7 @@ public class SystemTest {
 				processor.executeCommand();
 			}
 
-			FileHandler fileHandler = new FileHandler(
-					FILE_ERROR_LOG);
+			FileHandler fileHandler = new FileHandler(FILE_ERROR_LOG);
 			fileHandler.clearFile();
 			String fileName;
 			fileName = processor.getFeedbackFileName();
